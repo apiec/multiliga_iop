968b1aa37163dd0840347759080c4dd687e4176b
apiec <apieczul97@gmail.com>
2021-01-27 22:45:00 +0100

    minor refactor

--- MultiLiga-IOP/ClientApp/src/components/RaceDetailsPage.js
+++ MultiLiga-IOP/ClientApp/src/components/RaceDetailsPage.js
@@ -7,7 +7,6 @@ const RaceDetailsPage = (props) => {
   const [resultListDefault, setResultListDefault] = useState();
   const [resultList, setResultList] = useState();
   const [raceData, setRaceData] = useState();
-  const [disciplineNames, setDisciplineNames] = useState();
 
   const fetchResultData = async () => {
       return await fetch(`race/getresults${props.location.search}`)
@@ -28,10 +27,8 @@ const RaceDetailsPage = (props) => {
   }
 
   const updateInput = async (input) => {
-    const filtered = resultList.map(result => {
-      let newResult = {...result};
-      newResult.display = result.user.userName.toLowerCase().includes(input.toLowerCase());
-      return newResult;
+    const filtered = resultListDefault.map(result => {
+      return {...result, "display": result.user.userName.toLowerCase().includes(input.toLowerCase())};
     });
     setInput(input);
     setResultList(filtered);
