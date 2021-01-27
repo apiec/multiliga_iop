71208dd05a1810db08f8b765354a1bd7b5926732
apiec <apieczul97@gmail.com>
2021-01-28 00:05:57 +0100

    Update npm

---
+++ MultiLiga-IOP/ClientApp/src/components/RaceSignupButton.js
@@ -0,0 +1,27 @@
+﻿import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const RaceSignupButton = ({ userId, raceId }) => {
+    const [isSignedUp, setIsSignedUp] = useState(false);
+    
+    const signUp = async () => {
+        return await fetch(`race/signup?userId=${userId}&raceId=${raceId}`, {
+            method: 'POST'
+        })
+        .then(result => setIsSignedUp(result.ok))
+    } 
+
+    return (
+        isSignedUp ?
+        <>
+            <button onClick={signUp}>Sign up!</button>
+        </>
+        :
+        <>
+            <button onClick={signOut}>Sign out!</button>
+        </>
+        
+    );
+}
+
+export default RaceSignupButton
--- MultiLiga-IOP/ClientApp/src/components/RacesList.js
+++ MultiLiga-IOP/ClientApp/src/components/RacesList.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import authService from './api-authorization/AuthorizeService'
 import { Link } from 'react-router-dom';
+import RaceSignupButton from './RaceSignupButton'
 
 const RacesList = ({ racesList = [], seasonList = {}, leagueNames = {}, disciplineNames = {} }) => {
+    
+    const [userId, setUserId] = useState();
+
+    useEffect( () => {
+        const f = async () => {
+            await authService.getUser()
+            .then(user => setUserId(user.sub))
+        }
+        f();
+    }, [])
+    
     return (
         <>
             <table className='table table-striped' aria-labelledby="tableLabel">
@@ -11,6 +24,7 @@ const RacesList = ({ racesList = [], seasonList = {}, leagueNames = {}, discipli
                         <th>Season</th>
                         <th>League</th>
                         <th>Discipline</th>
+                        <th>Sign up</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -29,6 +43,9 @@ const RacesList = ({ racesList = [], seasonList = {}, leagueNames = {}, discipli
                                 </td>
                                 <td>
                                     <Link to={`/league/?disciplineId=${leagueNames[seasonList[race.seasonId].leagueId].disciplineId}`}> {disciplineNames[leagueNames[seasonList[race.seasonId].leagueId].disciplineId]} </Link>
+                                </td>
+                                <td>
+                                    <RaceSignupButton userId={userId} raceId={race.id}/>
                                 </td>
                             </>
                             :
